dcc5e7379ed27cccd10d5c84b57f199b975ec7e0
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-09-25 16:00:32 -0500

    Initial commit
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  url_api: 'https://store.nicobytes.site/api',
+  firebase: {
+    apiKey: 'AIzaSyCN599W6_flBEpe0xrcuBlGPRW3OKzDr7o',
+    authDomain: 'ng-mat-store-forms.firebaseapp.com',
+    databaseURL: 'https://ng-mat-store-forms.firebaseio.com',
+    projectId: 'ng-mat-store-forms',
+    storageBucket: 'ng-mat-store-forms.appspot.com',
+    messagingSenderId: '783669493891',
+    appId: '1:783669493891:web:f973c1ba9bb213e2c75bb6'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  url_api: 'https://store.nicobytes.site/api',
+  firebase: {
+    apiKey: 'AIzaSyCN599W6_flBEpe0xrcuBlGPRW3OKzDr7o',
+    authDomain: 'ng-mat-store-forms.firebaseapp.com',
+    databaseURL: 'https://ng-mat-store-forms.firebaseio.com',
+    projectId: 'ng-mat-store-forms',
+    storageBucket: 'ng-mat-store-forms.appspot.com',
+    messagingSenderId: '783669493891',
+    appId: '1:783669493891:web:f973c1ba9bb213e2c75bb6'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
