7edb60d43b3d9d7fc0fd4b171812e581189fe555
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-10-20 09:34:47 -0500

    Se muetstra el listado de categorias.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/admin/categories/components/categories/categories.component.ts
+++ src/app/admin/categories/components/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '@core/models/category.model';
+import { CategoriesService } from '@core/services/categories.service';
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[];
+  displayedColumns: string[] = ['id', 'name', 'image', 'actions'];
+
+
+  constructor(private cs: CategoriesService) { }
 
   ngOnInit(): void {
+    this.cs.getAllCategories()
+      .subscribe((categories: Category[]) => {
+        this.categories = categories;
+      });
   }
-
 }
